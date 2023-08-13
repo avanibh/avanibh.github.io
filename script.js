1bf1311c313ed7fc75f497801a6afe66f90e8686
avanibh <138714285+avanibh@users.noreply.github.com>
2023-08-12 21:08:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ document.addEventListener("DOMContentLoaded", function() {
     // Set the background GIF
     backgroundDiv.style.backgroundImage = `url(${randomGifFilename})`;
 });
+// Get the image element
+const backgroundImage = document.querySelector('.image-background');
+
+// Create a new Image object
+const img = new Image();
+img.src = backgroundImage.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
+img.crossOrigin = 'anonymous';
+
+// Calculate the perceived brightness of the image
+img.onload = function () {
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    const context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0, img.width, img.height);
+
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let sum = 0;
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        sum += imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2];
+    }
+    const averageBrightness = sum / (imageData.width * imageData.height * 3);
+
+    // Determine text color based on brightness
+    const textColor = averageBrightness > 127.5 ? 'black' : 'white';
+
+    // Set text color dynamically
+    const content = document.querySelector('.content');
+    content.style.color = textColor;
+};
